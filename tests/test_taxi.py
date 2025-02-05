b6154646162bbc0da5f3cc9cbbf0527457d6d37a
jruizndev <jorsqn@gmail.com>
2025-02-05 21:42:19 +0100

    test: add rate calculation test with state changes

--- tests/test_taxi.py
+++ tests/test_taxi.py
@@ -11,3 +11,10 @@ class TestTaximeter(unittest.TestCase):
     def test_calculate_rate_stopped(self):
         rate = calculate_rate(10, False)
         self.assertEqual(rate, 0.20)
+
+    # Test para probar el cálcu.lo de la tarifa total acumulada en un trayecto cambiado de estado
+    def test_acummulated_rate_with_state_change(self):
+        first_rate = calculate_rate(10, False)
+        second_rate = calculate_rate(10, True)
+        total_rate =first_rate + second_rate
+        self.assertEqual(total_rate, 0.70)
