7eee40d83b0c4e38549bc36e495c0d11ac80c370
jruizndev <jorsqn@gmail.com>
2025-02-06 20:24:07 +0100

    test: add normal rate test for taxi in motion

--- tests/test_taxi.py
+++ tests/test_taxi.py
@@ -31,3 +31,11 @@ class TestTaximeter(unittest.TestCase):
        first_rate = calculate_rate(10, False)
        second_rate = calculate_rate(10, True)
        self.assertEqual(first_rate + second_rate, expected_total)
+
+       # Test para comprobar la función de cálculo de tarfa en movimiento: tarifa normal
+   @patch('main.get_current_rate')
+   def test_calculate_rate_in_motion_normal(self, mock_get_rate):
+       mock_get_rate.return_value = TIME_SLOTS['normal']
+       expected_rate = 10 * TIME_SLOTS['normal']['motion_rate']  
+       rate = calculate_rate(10, True)
+       self.assertEqual(rate, expected_rate)
