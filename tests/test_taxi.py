0e6001432688d5f6e261fd4e1e03b68ff7f89e34
jruizndev <jorsqn@gmail.com>
2025-02-11 14:36:39 +0100

    Modify test_accumulated_reate_with_state_change_normal to provide fixed segment_time

--- tests/test_taxi.py
+++ tests/test_taxi.py
@@ -148,7 +148,6 @@ class TestTaximeter(unittest.TestCase):
             self.assertEqual(rate, expected_rate)
    
    # Validaciónfunción de cálculo de tarifa en parado: tarifa normal
-
     def test_calculate_rate_stopped_normal(self):
        with patch.object(RateCalculator, 'get_current_rate', return_value=TIME_SLOTS['normal']):
             self.trip.in_motion = False
@@ -158,17 +157,17 @@ class TestTaximeter(unittest.TestCase):
             expected_rate = 10 * TIME_SLOTS['normal']['stopped_rate']
             self.assertEqual(rate, expected_rate)
    
-   # Test para probar el cálculo de la tarifa total acumulada en un trayecto cambiado de estado: tarifa normal
-    @patch('main.get_current_rate')
-    def test_accumulated_rate_with_state_change_normal(self, mock_get_rate):
-       mock_get_rate.return_value = TIME_SLOTS['normal']
-       stopped = 10 * TIME_SLOTS['normal']['stopped_rate']  
-       in_motion = 10 * TIME_SLOTS['normal']['motion_rate']  
-       expected_total = stopped + in_motion   
+   # Validación de cáculo de tarifa con estado parado y en movimiento: tarifa normal
+    def test_accumulated_rate_with_state_change_normal(self):
+       with patch.object(RateCalculator, 'get_current_rate', return_value=TIME_SLOTS['normal']):
+            self.trip.in_motion = False
+            first_rate = self.trip.rate_calculator.calculate_rate(10, self.trip.in_motion)
 
-       first_rate = calculate_rate(10, False)
-       second_rate = calculate_rate(10, True)
-       self.assertEqual(first_rate + second_rate, expected_total)
+            self.trip.in_motion = True
+            second_rate = self.trip.rate_calculator.calculate_rate(10, self.trip.in_motion)
+
+            expected_total = first_rate + second_rate
+            self.assertEqual(first_rate + second_rate, expected_total)
 
    # Test para verificar multiplicador de tarifa en condición de lluvia
     @patch('main.get_current_rate')
