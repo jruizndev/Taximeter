18f5d2d332ba0f9a4e7abe5c2a7f7dc8807e1eec
jruizndev <jorsqn@gmail.com>
2025-02-12 08:58:56 +0100

    feat: add user registration to Auth class

--- auth/auth.py
+++ auth/auth.py
@@ -14,3 +14,27 @@ class Auth:
         salt = bcrypt.gensalt()
         password_hash = bcrypt.hashpw(password_bytes, salt)
         return password_hash
+
+    # Registro de usuario (username, password y role)
+    def register_user(self, username, password, role='driver'):
+        try:
+            self.db.connect()
+            cursor = self.db.connection.cursor()
+
+            # Verifica si el usuario ya existe
+            cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+            if cursor.fetchone():
+                return False, "El usuario ya existe"
+
+            # Crear nuevo usuario+
+            hashed_password = self.hash_password(password)
+            cursor.execute(
+                "INSERT INTO users (username, password, role) VALUES (%s, %s, %s)",
+                (username, hashed_password, role)
+            )
+            self.db.connection.commit()
+            return True, "Usuario creado correctamente"
+        except Exception as e:
+            return False, f"Error en registro: {str(e)}"
+        finally:
+            self.db.disconnect()
